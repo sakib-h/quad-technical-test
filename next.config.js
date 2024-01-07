3c85889a9a242932b33df5ae214389d26c574a95
Sakib Hasan <sakib100.sa@gmail.com>
2024-01-08 02:33:26 +0600

    some typo fixed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["www.api.technicaltest.quadtheoryltd.com"],
+        domains: ["https:www.api.technicaltest.quadtheoryltd.com"],
     },
 };
 
